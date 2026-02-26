a0b33359bf95ffc6415ccf311b143ec07a21be12
harsh08-rgb <hurshsoni83401@gmail.com>
2026-02-26 08:34:21 +0530

    uc2 commit

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,7 +1,18 @@
-public class PalindromeCheckerApp {
-    public static void main(String[] args) {
-        System.out.println("welcome to the palindrome checker management app");
-        System.out.println("version : 6");
-        System.out.println(" System intilized successfully");
+class PalindromeCheckerApp {
+    static void main() {
+        String str = "madam";
+        boolean isPalindrome = true;
+        for (int i = 0; i < str.length() / 2; i++){
+            if(str.charAt(i) !=str.charAt(str.length()-1-i)){
+                isPalindrome=false;
+                break;
+
+            }
+        }
+        if ( isPalindrome){
+            System.out.println(str + " is Palindrome");
+        } else {
+            System.out.println(str + " is not a palindrome");
+        }
     }
 }
