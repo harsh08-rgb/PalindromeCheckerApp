7dd991b22b238c77a348d60d7d712bbc0f115919
harsh08-rgb <hurshsoni83401@gmail.com>
2026-02-26 09:26:18 +0530

    uc6 commit

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,36 +1,24 @@
+import java.util.Queue;
+import java.util.LinkedList;
 import java.util.Stack;
 
 public class PalindromeCheckerApp {
-
-    /**
-     * Application entry point for UC5.
-     * @param args Command-line arguments
-     */
     public static void main(String[] args) {
 
-        // Declare and initialize the input string.
-        String input = "noon";
-
-        // Create a Stack to store characters.
+        String input = "civic";
+        Queue<Character> queue = new LinkedList<>();
         Stack<Character> stack = new Stack<>();
-
-        // Push each character of the string into the stack.
         for (char c : input.toCharArray()) {
+            queue.add(c);
             stack.push(c);
         }
-
-        // Assume palindrome initially.
         boolean isPalindrome = true;
-
-        // Iterate again through original string.
-        for (char c : input.toCharArray()) {
-            if (c != stack.pop()) {
+        while (!queue.isEmpty()) {
+            if (!queue.remove().equals(stack.pop())) {
                 isPalindrome = false;
                 break;
             }
         }
-
-        // Display output
         System.out.println("Input : " + input);
         System.out.println("Is Palindrome? : " + isPalindrome);
     }
